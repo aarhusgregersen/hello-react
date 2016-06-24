ea0abf2f70513457d0868c8210f648baa93858ab
Martin Gregersen JEF <mag@jef.dk>
2016-06-24 09:09:09 +0200

    Finishes adding basic components

--- public/app.jsx
+++ public/app.jsx
@@ -1,4 +1,26 @@
+var Greeter = React.createClass({
+  getDefaultProps: function() {
+    return {
+      name: 'React',
+      msg:  'This is a default component for msg in use'
+    };
+  },
+  render: function() {
+    var name = this.props.name;
+    var msg = this.props.msg;
+
+    return(
+      <div>
+        <h1>Hello {name}, this is rendered from javascript!</h1>
+        <p>{msg}</p>
+      </div>
+    );
+  }
+});
+
+var msg = 'This is a variable component in use';
+
 ReactDOM.render(
-  <h1>Hello react, this is rendered from javascript!</h1>,
+  <Greeter name="Martin Gregersen" msg={msg}/>,
   document.getElementById('app')
 );
