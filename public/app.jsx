406fe351a4538c6754b476803f99a7ddcc9b7cf9
Martin Gregersen JEF <mag@jef.dk>
2016-06-24 11:46:59 +0200

    Finalizes first part of tutorial. App is a little buggy

--- public/app.jsx
+++ public/app.jsx
@@ -1,49 +1,80 @@
-var Greeter = React.createClass({
-  getDefaultProps: function() {
-    return {
-      name: 'React',
-      msg:  'This is a default component for msg in use'
-    };
-  },
-  getInitialState: function() {
-    return {
-      name: this.props.name
-    };
-  },
-  onButtonClick: function(e) {
+var GreeterForm = React.createClass({
+  onFormSubmit: function(e) {
     e.preventDefault();
 
-    var nameRef = this.refs.name;
-    var name = nameRef.value;
-    nameRef.value = '';
+    var updates = {};
+    var name = this.refs.name.value;
+    var message = this.refs.message.value;
 
-    if (typeof name === 'string' && name.length > 0) {
-      this.setState({
-        name: name
-      });
-    };
+    if (name.length > 0) {
+      this.refs.name.value = '';
+      updates. name = name;
+    }
+
+    if (message.length > 0) {
+      this.refs.message.value = '';
+      updates.message = message;
+    }
+
+    this.props.onNewData(updates);
   },
   render: function() {
-    var name = this.state.name;
-    var msg = this.props.msg;
+    return (
+      <form onSubmit={this.onFormSubmit}>
+        <div>
+          <input type="text" ref="name" placeholder"What is your name?"/>
+          <textarea ref="message" placeholder="Enter message"/>
+          <button>Submit</button>
+        </div>
+      </form>
+    );
+  }
+});
 
-    return(
+var GreeterMessage = React.createClass({
+  render: function() {
+    var name = this.props.name;
+    var message = this.props.message;
+
+    return (
       <div>
         <h1>Hello {name}!</h1>
-        <p>{msg}</p>
-
-        <form onSubmit={this.onButtonClick}>
-          <input type="text" ref="name"/>
-          <button>Set name</button>
-        </form>
+        <p>{message}</p>
       </div>
     );
   }
 });
 
-var msg = 'This is a variable component in use';
+var Greeter = React.createClass({
+  getDefaultProps: function() {
+    return {
+      name: 'React',
+      message:  'This is a default component for msg in use'
+    };
+  },
+  getInitialState: function() {
+    return {
+      name: this.props.name,
+      message: this.props.message
+    };
+  },
+  handleNewData: function(updates) {
+    this.setState(updates);
+  },
+  render: function() {
+    var name = this.state.name;
+    var message = this.props.message;
+
+    return(
+      <div>
+      <GreeterMessage name={name} message={message}/>
+      <GreeterForm onNewData={this.handleNewData}/>
+      </div>
+    );
+  }
+});
 
 ReactDOM.render(
-  <Greeter name="Martin Gregersen" msg={msg}/>,
+  <Greeter name={name} message={message}/>,
   document.getElementById('app')
 );
